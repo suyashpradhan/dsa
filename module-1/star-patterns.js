ee99ca1acc0d94ae94faabcc93618d41842152ad
Suyash Pradhan <pradhangroup@hotmail.com>
2025-11-07 22:11:17 +0530

    complex patterns

--- module-1/star-patterns.js
+++ module-1/star-patterns.js
@@ -12,6 +12,8 @@ for (i = 0; i < 4; i++) {
   console.log(row);
 }
 
+console.log("--------------------");
+
 // Pattern 2
 // *
 // * *
@@ -21,6 +23,77 @@ for (i = 0; i < 4; i++) {
   let row = "";
 
   for (j = 0; j <= i; j++) {
+    row = row + "*";
+  }
+  console.log(row);
+}
+
+console.log("--------------------");
+
+// Pattern 3
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+for (i = 0; i < 5; i++) {
+  let row = "";
+  for (j = 0; j <= i; j++) {
+    row = row + (j + 1);
+  }
+  console.log(row);
+}
+
+console.log("--------------------");
+
+// Pattern 4
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
+
+for (i = 0; i < 5; i++) {
+  let row = "";
+  for (j = 0; j <= i; j++) {
+    row = row + (i + 1);
+  }
+  console.log(row);
+}
+
+console.log("--------------------");
+
+// Pattern 5
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+for (i = 4; i >= 0; i--) {
+  let row = "";
+  for (j = 0; j <= i; j++) {
+    row = row + (j + 1);
+  }
+  console.log(row);
+}
+
+console.log("--------------------");
+
+// Pattern 6
+// - - - - *
+// - - - * *
+// - - * * *
+// - * * * *
+// * * * * *
+
+let n = 5;
+for (i = 0; i < n; i++) {
+  let row = "";
+  for (j = 0; j < n - (i + 1); j++) {
+    row = row + " ";
+  }
+  for (k = 0; k <= i; k++) {
     row = row + "*";
   }
   console.log(row);
